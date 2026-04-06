1c98766fa59fb46253f5e72e1dca56ef7dfd9b54
Gilson Siqueira <3647085+barateza@users.noreply.github.com>
2026-04-06 20:23:07 -0300

    Remove vendored TurboQuant folder

--- tonbistudio-turboquant-pytorch/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    from .turboquant import TurboQuantMSE, TurboQuantProd, TurboQuantKVCache
-    from .lloyd_max import LloydMaxCodebook, solve_lloyd_max
-    from .compressors import TurboQuantCompressorV2, TurboQuantCompressorMSE
-except Exception:
-    # Allow importing the package modules when running tests directly
-    from turboquant import TurboQuantMSE, TurboQuantProd, TurboQuantKVCache
-    from lloyd_max import LloydMaxCodebook, solve_lloyd_max
-    from compressors import TurboQuantCompressorV2, TurboQuantCompressorMSE
